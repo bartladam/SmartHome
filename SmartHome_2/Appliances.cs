2ab00a60e316d91e4b5f1ee0d7ba7b0c668c6dd0
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-12 13:59:51 +0200

    Appliances class complete

--- SmartHome_2/Appliances.cs
+++ SmartHome_2/Appliances.cs
@@ -8,5 +8,27 @@ namespace SmartHome_2
 {
     internal class Appliances
     {
+        /// <summary>
+        /// Appliance has always name for recognize what we turn on / off
+        /// </summary>
+        public string nameAppliance { get; private set; }
+        /// <summary>
+        /// Is appliance active or not
+        /// </summary>
+        public bool applianceWork { get; private set; }
+        public Appliances(string nameAppliance)
+        {
+            this.nameAppliance = nameAppliance;
+        }
+        /// <summary>
+        /// User remotly control aplliance 
+        /// </summary>
+        /// <param name="turnOn"></param>
+        /// <returns></returns>
+        public bool StartAppliance(bool turnOn)
+        {
+            applianceWork = turnOn;
+            return applianceWork;
+        }
     }
 }
