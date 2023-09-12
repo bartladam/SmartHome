e03908d7c5d46816ffb5260173ec6c56ea7f1b63
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-12 13:58:44 +0200

    House class complete

--- SmartHome_2/House.cs
+++ SmartHome_2/House.cs
@@ -8,5 +8,24 @@ namespace SmartHome_2
 {
     internal class House
     {
+        /// <summary>
+        /// Each smart house has application which is listening command remotely by mobile phone
+        /// </summary>
+        public System System { get; private set; }
+        /// <summary>
+        ///  House has rooms which are controled by system
+        /// </summary>
+        public Room[] rooms { get; private set; }
+        public House(params Room[] rooms)
+        {
+            System = new System(rooms);
+        }
+        /// <summary>
+        /// Show system interface focused on controlling house
+        /// </summary>
+        public void OpenUserInterface()
+        {
+            System.UserInterface();
+        }
     }
 }
