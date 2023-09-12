484db3c94f9e625d0bbcabc42f72dade68411c1a
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-12 14:11:18 +0200

    project complete

--- SmartHome_2/Program.cs
+++ SmartHome_2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SmartHome_2;
+
+MobilePhone mobile = new MobilePhone("AdamPhone", 58885544);
+House house = new House(
+    new Hallway("Hallway", 7),
+    new Bathroom("Bathroom", 12, new Appliances("digital mirror"), new Appliances("toothbrush")),
+    new LivingRoom("Living room", 50, new Appliances("Television")),
+    new Kitchen("Kitchen", 30, new Appliances("Coffe maker"))
+    );
+mobile.OpenHouseSystem(house);
