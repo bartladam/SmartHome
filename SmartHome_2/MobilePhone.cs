5790e574628a80c2ecace4e270483cb0f738238a
Adam46072 <adam.bartl2001@seznam.cz>
2023-09-12 13:48:34 +0200

    mobile phone class complete

--- SmartHome_2/MobilePhone.cs
+++ SmartHome_2/MobilePhone.cs
@@ -8,5 +8,26 @@ namespace SmartHome_2
 {
     internal class MobilePhone
     {
+        /// <summary>
+        /// We used for recognize which mobile phone is controlling house system
+        /// </summary>
+        public string nameMobilePhone { get; private set; }
+        /// <summary>
+        /// More specific detail than name mobile phone. This attributes say: which device is controlling house system
+        /// </summary>
+        public uint serialNumber { get; private set; }
+        public MobilePhone(string nameMobilePhone, uint serialNumber)
+        {
+            this.nameMobilePhone = nameMobilePhone;
+            this.serialNumber = serialNumber;
+        }
+        /// <summary>
+        /// Mobile phone has application for controlling house. 
+        /// </summary>
+        /// <param name="house"></param>
+        public void OpenHouseSystem(House house)
+        {
+            house.OpenUserInterface();
+        }
     }
 }
